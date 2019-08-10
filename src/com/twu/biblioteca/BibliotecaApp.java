7936aa7b0e92c265df64e155eaf266b19a1f2138
Annie Zhou <a.zhou@outlook.com>
2019-08-10 19:50:44 +1000

    [1.1] Show welcome message on starting

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,25 @@
 package com.twu.biblioteca;
 
+import java.io.PrintStream;
+
 public class BibliotecaApp {
+    PrintStream printer;
+    static final String WELCOME_MSG = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
+
+    public BibliotecaApp() {
+        this.printer = System.out;
+    }
+
+    public BibliotecaApp(PrintStream printer) {
+        this.printer = printer;
+    }
+
+    public void start() {
+        this.printer.println(WELCOME_MSG);
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        BibliotecaApp app = new BibliotecaApp();
+        app.start();
     }
 }
